998ad8899e5b4e67feef84d679bb993e166f48af
SeoJaeWan <sjw7324@naver.com>
2020-07-29 19:51:14 +0900

    슬라이더 함수 코드 정리 완료
    화면에 따라 프로필 페이지 width 자동 조정 ,
    화면 비율에 따라 출력이 바뀌도록 함수 조정

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,11 @@
-let current = 1;
-let move = -1080;
-let profiles;
-let profile_box;
+// 2020 7 27 서재완
+//  슬라이더 기능 구현
+let current = 1; // 표시할 페이지
+let move = -20; // 좌우 이동 비율
+let profiles; // 모든 표시될 페이지
+let profile_box; // 표시될 페이지 틀
+
+// 좌우 방향을 받아와 상황에 따라 20%씩 이동
 const onSlider = (attendants) => {
   profiles = document.querySelectorAll(".testimonials-profile");
   profile_box = document.querySelector(".testimonials-box");
@@ -11,32 +15,31 @@ const onSlider = (attendants) => {
 
   if (attendants === "left") {
     current++;
-    move = move - 1080;
+    move = move - 20;
     if (current > 3) {
-      changeProfile(-1080);
+      changeProfile(-20);
     }
   } else if (attendants === "right") {
     current--;
-    move = move + 1080;
+    move = move + 20;
     if (current < 1) {
-      changeProfile(-3240);
+      changeProfile(-60);
     }
   }
-  profile_box.style.transform = "translateX(" + move + "px)";
+  profile_box.style.transform = "translateX(" + move + "%)";
   profiles[current].classList.add("active");
 };
 
+// 좌우 제일 마지막에 도달했을 떄 순환시켜주는 함수
 const changeProfile = (change_move) => {
-  let before = current;
-  profiles[Math.abs(before - 3)].classList.add("active");
+  let temp_current = Math.abs(current - 3);
+  profiles[temp_current].classList.add("active");
 
   setTimeout(() => {
     move = change_move;
-    console.log(move);
-    current = Math.abs(before - 3);
+    profiles[current].classList.remove("active");
+    current = temp_current;
     profile_box.style =
-      " transition: all 0s; transform: translateX(" + move + "px)";
-
-    profiles[before].classList.remove("active");
+      " transition: all 0s; transform: translateX(" + move + "%)";
   }, 500);
 };
