3a0befd3880ed5e34428242b2f044993109583dc
SeoJaeWan <sjw7324@naver.com>
2020-07-29 20:20:26 +0900

    슬라이드 버튼 중복 클릭 방지
    슬라이드 중복 클릭 방지하기 위해서 조건식 추가

--- js/slider.js
+++ js/slider.js
@@ -5,29 +5,32 @@ let move = -20; // 좌우 이동 비율
 let profiles; // 모든 표시될 페이지
 let profile_box; // 표시될 페이지 틀
 
+let loading = true; // 중복 클릭 방지
+
 // 좌우 방향을 받아와 상황에 따라 20%씩 이동
 const onSlider = (attendants) => {
-  profiles = document.querySelectorAll(".testimonials-profile");
-  profile_box = document.querySelector(".testimonials-box");
+  if (loading) {
+    loading = false;
+    profiles = document.querySelectorAll(".testimonials-profile");
+    profile_box = document.querySelector(".testimonials-box");
 
-  profile_box.style = "transition: all 0.5s";
-  profiles[current].classList.remove("active");
+    profile_box.style = "transition: all 0.5s";
+    profiles[current].classList.remove("active");
 
-  if (attendants === "left") {
-    current++;
-    move = move - 20;
-    if (current > 3) {
-      changeProfile(-20);
+    if (attendants === "right") {
+      current++;
+      move = move - 20;
+      if (current > 3) changeProfile(-20);
+    } else if (attendants === "left") {
+      current--;
+      move = move + 20;
+      if (current < 1) changeProfile(-60);
     }
-  } else if (attendants === "right") {
-    current--;
-    move = move + 20;
-    if (current < 1) {
-      changeProfile(-60);
-    }
+
+    if (current <= 3 && current >= 1) loading = true;
+    profile_box.style.transform = "translateX(" + move + "%)";
+    profiles[current].classList.add("active");
   }
-  profile_box.style.transform = "translateX(" + move + "%)";
-  profiles[current].classList.add("active");
 };
 
 // 좌우 제일 마지막에 도달했을 떄 순환시켜주는 함수
@@ -41,5 +44,7 @@ const changeProfile = (change_move) => {
     current = temp_current;
     profile_box.style =
       " transition: all 0s; transform: translateX(" + move + "%)";
-  }, 500);
+
+    loading = true;
+  }, 400);
 };
