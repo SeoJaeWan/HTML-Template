a4bbd1e2ca5e77a2a6dbf651e89e0acfa7313756
SeoJaeWan <sjw7324@gmail.com>
2020-08-03 13:58:34 +0900

    배너 애니메이션 & 텍스트 애니메이션 추가
    텍스트 색 바꾸는 애니메이션 추가 필요

--- js/banner_change.js
+++ js/banner_change.js
@@ -1,8 +1,13 @@
 var state = 0;
 var before_state = 0;
 var banner = document.querySelectorAll(".banner-img");
+var banner_text_box = document.querySelector(".banner-text");
+var banner_text = document.querySelectorAll(
+  ".banner-text .banner-text-underline"
+);
 
 function banner_change() {
+  banner_text[state].style.transform = "scale(1)";
   state++;
   if (state === 4) {
     state = 0;
@@ -10,14 +15,19 @@ function banner_change() {
   }
   setTimeout(function () {
     console.log(state);
-    banner[before_state].style.transform = "translateY(930px)";
-    banner[state].style.transform = "translateY(0px)";
+    banner[before_state].classList.remove("banner-abled");
+    banner[state].classList.remove("banner-disabled");
+
+    banner[before_state].classList.add("banner-disabled");
+    banner[state].classList.add("banner-abled");
     //        banner.classList[state-1].toggle("abled","disabled");
     //        banner.classList[state].add("abled");
 
+    banner_text[before_state].style.transform = "scale(0)";
     before_state = state;
     banner_change();
   }, 5000);
 }
-banner[0].style.transform = "translateY(0px)";
+// banner[0].style.transform = "translateY(0px)";
+banner_text_box.style.transform = "translate(-50%, -50px)";
 banner_change();
