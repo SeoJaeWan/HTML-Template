7f8a07b426abc092e0dfc9b66d1dd0c77dc0d7c3
SeoJaeWan <sjw7324@naver.com>
2020-08-05 22:22:50 +0900

    스크롤 이벤트 추가
    스크롤을 내렸을 때 버튼이 나오도록 이벤트를 추가하였다.
    
    이제 portfolio 이벤트를 하고 css 작업을 하자!

--- js/banner_change.js
+++ js/banner_change.js
@@ -6,46 +6,37 @@ var banner_text_underline = document.querySelectorAll(
   ".banner-text .banner-text-underline"
 );
 var banner_text_opacity = document.querySelectorAll(".banner-text-opacity");
-var banner_text = document.querySelectorAll(".banner-text-item");
+var banner_text = document.querySelectorAll(".banner-text-item a");
 
 function banner_change() {
-  banner_text_underline[0].style.transform = "scale(1)";
-  banner_text_box.style.transform = "translate(-50%, -50px)";
-  banner_text[0].classList.add("half-active");
-  banner_text_opacity[0].classList.add("change-width");
-
   setInterval(function () {
     state++;
     if (state === 4) {
       state = 0;
       before_state = 3;
     }
-    console.log(state);
     banner[before_state].classList.remove("banner-abled");
     banner[state].classList.remove("banner-disabled");
 
     banner[before_state].classList.add("banner-disabled");
     banner[state].classList.add("banner-abled");
-    //        banner.classList[state-1].toggle("abled","disabled");
-    //        banner.classList[state].add("abled");
 
-    banner_text_underline[before_state].style.transform = "scale(0)";
-    banner_text[before_state].classList.remove("half-active");
+    banner_text_underline[before_state].classList.remove("change-scale");
+    banner_text[before_state * 2].classList.remove("half-active");
     banner_text_opacity[before_state].classList.remove("change-width");
 
     before_state = state;
 
     setTimeout(function () {
-      banner_text[state].classList.add("half-active");
+      banner_text[state * 2].classList.add("half-active");
       banner_text_opacity[state].classList.add("change-width");
 
-      banner_text_underline[state].style.transform = "scale(1)";
+      banner_text_underline[state].classList.add("change-scale");
 
       console.log(state, before_state);
-      console.log(banner_text[state]);
+      console.log(banner_text[state * 2]);
     }, 3000);
   }, 10000);
 }
-// banner[0].style.transform = "translateY(0px)";
-
+banner_text_box.style.transform = "translate(-50%, -50px)";
 banner_change();
